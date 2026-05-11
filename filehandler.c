09621f38d85b0a77d07d50f0b51eda35a571f648
Sierra-Bakery <150887228+Sierra-Bakery@users.noreply.github.com>
2026-05-11 20:42:41 +0800

    refactor: enhance comments for clarity in handle_file and free_map functions

--- filehandler.c
+++ filehandler.c
@@ -12,28 +12,28 @@ int handle_file(const char *filename, MapData *data)
 
     if (fp == NULL)
     {
-        fprintf(stderr, "Error: Could not open file \"%s\"\n", filename);
+        fprintf(stderr, "Error: Could not open file \"%s\"\n", filename); /* Print error message if file cannot be opened */
         success = 0;
     }
     else
     {
-        fscanf(fp, "%d %d", &data->sizeR, &data->sizeC);
+        fscanf(fp, "%d %d", &data->sizeR, &data->sizeC); /* Read the number of rows and columns from the file */
 
-        data->map = (int **)malloc(data->sizeR * sizeof(int *));
+        data->map = (int **)malloc(data->sizeR * sizeof(int *)); /* Allocate memory for the map based on the number of rows */
 
         for (r = 0; r < data->sizeR; r++)
         {
             int c;
-            data->map[r] = (int *)malloc(data->sizeC * sizeof(int));
+            data->map[r] = (int *)malloc(data->sizeC * sizeof(int)); /* Allocate memory for each row of the map */
 
             for (c = 0; c < data->sizeC; c++)
             {
-                fscanf(fp, "%d", &data->map[r][c]);
+                fscanf(fp, "%d", &data->map[r][c]); /* Read each element of the map from the file */
             }
         }
 
         fclose(fp);
-        success = 1;
+        success = 1; /* Set success to 1 if the file was read successfully */
     }
 
     return success;
@@ -45,7 +45,8 @@ void free_map(MapData *data)
 
     for (r = 0; r < data->sizeR; r++)
     {
-        free(data->map[r]);
+        free(data->map[r]); /* Free memory allocated for each row of the map */\
+        data->map[r] = NULL; /* Set the pointer to NULL after freeing */
     }
 
     free(data->map);
