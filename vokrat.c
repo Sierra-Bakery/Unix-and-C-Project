1ef2fd2749a0c29e1a733f2ac0cc3043eade5952
Sierra-Bakery <150887228+Sierra-Bakery@users.noreply.github.com>
2026-05-11 21:26:42 +0800

    feat: add display and color source files to makefile and update vokrat.c for display functionality

--- vokrat.c
+++ vokrat.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "filehandler.h"
+#include "display.h"
 
 int main(int argc, char *argv[])
 {
@@ -13,23 +14,11 @@ int main(int argc, char *argv[])
     }
     else
     {
-        int ok = handle_file(argv[1], &data); /* Call the file handling function to read the map data */
-
+        int ok = handle_file(argv[1], &data);
+ 
         if (ok)
         {
-            int r;
-            printf("Rows: %d, Cols: %d\n", data.sizeR, data.sizeC); /* Print the number of rows and columns in the map */
-
-            for (r = 0; r < data.sizeR; r++)
-            {
-                int c;
-                for (c = 0; c < data.sizeC; c++)
-                {
-                    printf("%d ", data.map[r][c]); /* Print each element in the map */
-                }
-                printf("\n");
-            }
-
+            display_map(&data);
             free_map(&data);
             result = 0;
         }
@@ -41,3 +30,5 @@ int main(int argc, char *argv[])
 
     return result;
 }
+
+/* when i awake the morning, i will be a better programmer than i am now. */
