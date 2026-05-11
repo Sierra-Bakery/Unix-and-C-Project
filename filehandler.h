4875f5157949bdad3b15161338a5f6e048b7b801
Sierra-Bakery <150887228+Sierra-Bakery@users.noreply.github.com>
2026-05-11 19:03:11 +0800

    refactor: update comments for clarity in filehandler.h

--- filehandler.h
+++ filehandler.h
@@ -8,10 +8,10 @@ typedef struct
     int **map;
 } MapData;
 
-/* Returns 1 on success, 0 on failure */
+/* shows 1 for working, 0 for failure */
 int handle_file(const char *filename, MapData *data);
 
-/* Always call this after handle_file succeeds, to free map memory */
+/* free memory from the map data */
 void free_map(MapData *data);
 
 #endif /* FILEHANDLER_H */
