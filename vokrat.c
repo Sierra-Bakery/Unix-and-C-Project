0daaff2840e3aa43403ec487eae8aa3c37454c77
Sierra-Bakery <150887228+Sierra-Bakery@users.noreply.github.com>
2026-05-12 13:20:29 +0800

    feat: refactor main function to use separate size parameters and dynamic map allocation

--- vokrat.c
+++ vokrat.c
@@ -5,21 +5,23 @@
 int main(int argc, char *argv[]) /* Takes command line arguments to processes the map file */
 {
     int result;
-    MapData data; /* Start map data structure (3 variables; sizeR, sizeC, map) */
+    int sizeR;
+    int sizeC;
+    int **map;
 
-    if (argc != 2)
+    if (argc != 2) /* Check if the correct number of command line arguments is provided */
     {
         fprintf(stderr, "Usage: %s <map_file>\n", argv[0]); /* Print usage message if command line arguments are not correct */
         result = 1;
     }
     else
     {
-        int ok = handle_file(argv[1], &data); /* Process the map and store the data in the map data structure. Returns 1 if it worked*/
+        int ok = handle_file(argv[1], &sizeR, &sizeC, &map); /* Process the map and store the data in the map data structure. Returns 1 if it worked*/
  
         if (ok)
         {
-            display_map(&data);
-            free_map(&data);
+            display_map(sizeR, sizeC, map);
+            free_map(sizeR, map);
             result = 0;
         }
         else
