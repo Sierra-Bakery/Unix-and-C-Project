f4587019cacdb988a223c0b8846c2976cf195193
Sierra-Bakery <150887228+Sierra-Bakery@users.noreply.github.com>
2026-05-13 16:36:02 +0800

    feat: refactor player movement to use input loop and enhance enemy movement logic

--- game.c
+++ game.c
@@ -3,70 +3,110 @@
 #include <string.h>
 #include "game.h"
 #include "display.h"
+#include "random.h"
 
-static void move_player(int **map,
-                        int *playerR, int *playerC,
-                        char direction)
+static void move_enemy(int **map,
+                       int *enemyR, int *enemyC)
 {
     int newR;
     int newC;
     int blocked;
+    int input;
+    int inputValid = 0; /* 0 for invalid input, 1 for valid input */
  
-    newR = *playerR;
-    newC = *playerC;
+    newR = *enemyR;
+    newC = *enemyC;
  
-    if (direction == 'w') { newR = *playerR - 1; }
-    else if (direction == 's') { newR = *playerR + 1; }
-    else if (direction == 'a') { newC = *playerC - 1; }
-    else if (direction == 'd') { newC = *playerC + 1; }
- 
-    blocked = (map[newR][newC] == 1);
- 
-    if (!blocked)
+    while (inputValid == 0)
     {
-        map[*playerR][*playerC] = 0;
-        map[newR][newC]         = 4;
-        *playerR = newR;
-        *playerC = newC;
-    }
-    else
-    {
-        printf("Blocked!\n");
+        input = random_int(0, 3);
+        if (input == 0)
+        {
+            newR = *enemyR - 1;
+        }
+        else if (input == 1) 
+        { 
+            newR = *enemyR + 1; 
+        }
+        else if (input == 2) 
+        { 
+            newC = *enemyC - 1; 
+        }
+        else if (input == 3) 
+        { 
+            newC = *enemyC + 1; 
+        }
+
+        blocked = (map[newR][newC] == 1);
+ 
+        if (!blocked)
+        {
+            map[*enemyR][*enemyC] = 0;
+            map[newR][newC]         = 4;
+            *enemyR = newR;
+            *enemyC = newC;
+            inputValid = 1;
+        }
     }
 }
 
+static void move_player(int **map,
+                        int *playerR, int *playerC,
+                        char input)
+{
+    int newR;
+    int newC;
+    int blocked;
+    int inputValid = 0; /* 0 for invalid input, 1 for valid input */
+ 
+    newR = *playerR;
+    newC = *playerC;
+
+    while (inputValid == 0)
+    {
+        scanf(" %c", &input);
+        if (input == 'w')
+        {
+            newR = *playerR - 1;
+        }
+        else if (input == 's') 
+        { 
+            newR = *playerR + 1; 
+        }
+        else if (input == 'a') 
+        { 
+            newC = *playerC - 1; 
+        }
+        else if (input == 'd') 
+        { 
+            newC = *playerC + 1; 
+        }
+        
+        blocked = (map[newR][newC] == 1);
+ 
+        if (!blocked)
+        {
+            map[*playerR][*playerC] = 0;
+            map[newR][newC]         = 4;
+            *playerR = newR;
+            *playerC = newC;
+            inputValid = 1;
+        }
+    }
+
+ 
+
+}
+
 /* Processes one tick of the game */
-int gameTick(int **map, int playerR, int playerC, int goalR, int goalC, int treasureR, int treasureC, int enemyR, int enemyC, int enemyAggro, int playerHasTreasure, int *continueGame)
+int gameTick(int **map, int playerR, int playerC, int goalR,
+             int goalC, int treasureR, int treasureC, int enemyR,
+             int enemyC, int enemyAggro, int playerHasTreasure, int *continueGame)
 {
     char input;
     int inputValid = 0; /* 0 for invalid input, 1 for valid input */
 
-    while (!inputValid)
-    {
-        /* Get user input */
-        scanf(" %c", &input);
-        /* Process user input */
-        switch (input)
-        {
-            case 'W':
-            case 'w':
-                /* Move player up */
 
-            case 'S':
-            case 's':
-                /* Move player down */
 
-            case 'A':
-            case 'a':
-                /* Move player left */
 
-            case 'D':
-            case 'd':
-                /* Move player right */
-
-            default:
-                inputValid = 0; /* Invalid input */
-        }
-    }
-    return 0;
 }
