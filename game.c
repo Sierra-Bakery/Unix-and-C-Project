3cc07cf1fc36781619aebb0e4fe215402f7134cf
Sierra-Bakery <150887228+Sierra-Bakery@users.noreply.github.com>
2026-05-15 18:07:56 +0800

    feat: refactor player movement to accept input as a parameter and remove terminal buffer functions

--- game.c
+++ game.c
@@ -2,33 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <termios.h>
 #include <time.h>
 #include "game.h"
 #include "display.h"
 #include "random.h"
 #include "history.h"
-
-/* NOTE: This file contains functions from Curtin University's Assignment supplementary materials */
-/* Credit - Curtin University Assignment supplementary materials: How to Make a Program Accept a Char Immediately.zip */
-static void disableBuffer(void)
-{
-    struct termios mode;
-
-    tcgetattr(0, &mode);
-    mode.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(0, TCSANOW, &mode);
-}
-
-static void enableBuffer(void)
-{
-    struct termios mode;
-
-    tcgetattr(0, &mode);
-    mode.c_lflag |= (ECHO | ICANON);
-    tcsetattr(0, TCSANOW, &mode);
-}
-/* End of code from Curtin University's Assignment supplementary materials: How to Make a Program Accept a Char Immediately.zip */
 
 static void newSleep(float timeInSeconds)
 {
@@ -102,55 +80,49 @@ static void move_enemy(int **map, int rows, int cols,
 }
  
 static void move_player(int **map, int rows, int cols,
-                        int *playerR, int *playerC)
+                        int *playerR, int *playerC, char input)
 {
     int newR;
     int newC;
     int blocked;
-    char input;
     int inputValid = 0; /* 0 for invalid input, 1 for valid input */
+    
+    newR = *playerR;
+    newC = *playerC;
  
-    while (inputValid == 0)
+    if (input == 'w')
     {
-        newR = *playerR;
-        newC = *playerC;
+        newR = *playerR - 1;
+    }
+    else if (input == 's')
+    {
+        newR = *playerR + 1;
+    }
+    else if (input == 'a')
+    {
+        newC = *playerC - 1;
+    }
+    else if (input == 'd')
+    {
+        newC = *playerC + 1;
+    }
+    else
+    {
+        return; /* Invalid input, don't move the player */
+    }
  
-        disableBuffer();
-        scanf(" %c", &input);
-        enableBuffer();
-
-        /* Get input for undo*/
+    /* Bounds check BEFORE accessing map to prevent segfault */
+    if (newR >= 0 && newR < rows && newC >= 0 && newC < cols)
+    {
+        blocked = (map[newR][newC] == 1);
  
-        if (input == 'w')
+        if (!blocked)
         {
-            newR = *playerR - 1;
-        }
-        else if (input == 's')
-        {
-            newR = *playerR + 1;
-        }
-        else if (input == 'a')
-        {
-            newC = *playerC - 1;
-        }
-        else if (input == 'd')
-        {
-            newC = *playerC + 1;
-        }
- 
-        /* Bounds check BEFORE accessing map to prevent segfault */
-        if (newR >= 0 && newR < rows && newC >= 0 && newC < cols)
-        {
-            blocked = (map[newR][newC] == 1);
- 
-            if (!blocked)
-            {
-                map[*playerR][*playerC] = 0;
-                map[newR][newC]         = 4;
-                *playerR = newR;
-                *playerC = newC;
-                inputValid = 1;
-            }
+            map[*playerR][*playerC] = 0;
+            map[newR][newC]         = 4;
+            *playerR = newR;
+            *playerC = newC;
+            inputValid = 1;
         }
     }
     return;
@@ -188,11 +160,11 @@ static void check_goal(int playerR, int playerC, int goalR, int goalC, int playe
 int gameTick(int **map, int rows, int cols, int *playerR, int *playerC,
              int goalR, int goalC, int treasureR, int treasureC,
              int *enemyR, int *enemyC, int *enemyAggro, int *playerHasTreasure,
-             int *continueGame, int *enemyDirection, int *tileBelow)
+             int *continueGame, int *enemyDirection, int *tileBelow, char *input)
 {
     int i;
 
-    move_player(map, rows, cols, playerR, playerC);
+    move_player(map, rows, cols, playerR, playerC, input);
     check_caught(*playerR, *playerC, *enemyR, *enemyC, continueGame);
     check_treasure(*playerR, *playerC, treasureR, treasureC, playerHasTreasure, enemyAggro);
     check_goal(*playerR, *playerC, goalR, goalC, *playerHasTreasure, continueGame);
